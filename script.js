c2b79f135fe1615aa3c9cdd8f01d22ded372a15a
André Henrique da Silva Meliunas <81687022+andrehenriquemeliunas@users.noreply.github.com>
2021-04-20 21:09:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var poke = prompt('Escolha um número entre 1 e 4')
+if(poke == 1){
+    document.body.style.backgroundImage = "url(midias/poke1.0.png)"
+    document.write("<h1>Charmander</h1>")
+}else if(poke == 2){
+    document.body.style.backgroundImage = "url(midias/poke2.0.png)"
+    document.write("<h1>Bubassauro</h1>")
+}else if(poke == 3){
+    document.body.style.backgroundImage = "url(midias/poke3.0.png)"
+    document.write("<h1>Squirtle</h1>")
+}else if(poke == 4){
+    document.body.style.backgroundImage = "url(midias/poke4.1.png)"
+    document.write("<h1>Pikachu</h1>")
+}
